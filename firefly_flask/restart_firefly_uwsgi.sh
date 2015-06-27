42a6d14df44e1d4317989305bffd7399688e6ac1
Markus Rekkenbeil <hackathon@rekkenbeil.de>
2015-06-28 01:59:44 +0200

    Change restart script to tmux using

--- firefly_flask/restart_firefly_uwsgi.sh
+++ firefly_flask/restart_firefly_uwsgi.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
+tmux kill-session -t uwsgi_firefly
 pkill -f uwsgi
-sleep 1;
-screen -dmS firefly_flask uwsgi --socket /tmp/firefly_flask.sock --chdir=/srv/nginx/public_html/trivago_hackathon/project/firefly_flask/ --wsgi-file manage.py --   chmod-socket=666 --module manage --callable app
-sleep 2;
+sleep 3;
+tmux new-session -d -s uwsgi_firefly "uwsgi --socket /tmp/firefly_flask.sock --chdir=/srv/nginx/public_html/trivago_hackathon/project/firefly_flask/ --wsgi-file manage.py --chmod-socket=666 --module manage --callable app"
