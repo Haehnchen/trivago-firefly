4c7aa0f0bdf754bb5fef87e1b851b391fc61b42c
t7l <tobias.siebenlist@gmail.com>
2015-06-27 20:26:06 +0200

    redirect problem solved

--- firefly_flask/app/main/views.py
+++ firefly_flask/app/main/views.py
@@ -13,22 +13,11 @@ def index():
     else:
         return render_template('index.html',form = form)
 
-@main.route('/search/', methods=['GET', 'POST'])
 @main.route('/search/<search_query>', methods=['GET', 'POST'])
 def search(search_query = None):
-    if search_query:
-        form = SearchForm()
-        form.search_query.data = search_query
-        db_result = Search.query.join(Photo).filter(Search.id==Photo.search_id).filter(Search.search_string == search_query).all()
-
-        if form.validate_on_submit():
-            return redirect(url_for('main.search',search_query = form.search_query.data))
-        else:
-            return render_template('search.html',form = form,db_result = db_result)
-
-        form.search_query.data = search_query
-        return render_template('search.html',form = form,db_result = db_result, query = search_query)
-    else:
-        form = SearchForm()
-        return render_template('search.html',form = form)
-
+    form = SearchForm()
+    if form.validate_on_submit():
+        return redirect(url_for('main.search', search_query=form.search_query.data))
+    form.search_query.data = search_query
+    db_result = Search.query.join(Photo).filter(Search.id==Photo.search_id).filter(Search.search_string == search_query).all()
+    return render_template('search.html',form = form,db_result = db_result, query = search_query)
