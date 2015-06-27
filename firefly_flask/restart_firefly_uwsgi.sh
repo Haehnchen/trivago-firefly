3783562fac2e88c4ec0765ddcfd62fb1a0831855
Markus Rekkenbeil <hackathon@rekkenbeil.de>
2015-06-28 01:52:51 +0200

    Add generic startup script for uwsgi job

---
+++ firefly_flask/restart_firefly_uwsgi.sh
@@ -0,0 +1,5 @@
+!#/bin/sh
+pkill -f uwsgi
+sleep 1;
+screen -dmS firefly_flask uwsgi --socket /tmp/firefly_flask.sock --chdir=/srv/nginx/public_html/trivago_hackathon/project/firefly_flask/ --wsgi-file manage.py --   chmod-socket=666 --module manage --callable app
+sleep 2;
