1b91d1cb5166d5081dfa72464d4fe39966eb22f7
Markus Rekkenbeil <hackathon@rekkenbeil.de>
2015-06-28 02:07:02 +0200

    Add logging to the uwsgi process

--- firefly_flask/restart_firefly_uwsgi.sh
+++ firefly_flask/restart_firefly_uwsgi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tmux kill-session -t uwsgi_firefly
 sleep 3;
-tmux new-session -d -s uwsgi_firefly "uwsgi --socket /tmp/firefly_flask.sock --chdir=/srv/nginx/public_html/trivago_hackathon/project/firefly_flask/ --wsgi-file manage.py --chmod-socket=666 --module manage --callable app"
+tmux new-session -d -s uwsgi_firefly "uwsgi --socket /tmp/firefly_flask.sock --chdir=/srv/nginx/public_html/trivago_hackathon/project/firefly_flask/ --wsgi-file manage.py --chmod-socket=666 --module manage --callable app --logto /tmp/firefly_flask.log"
