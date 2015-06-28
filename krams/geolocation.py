7a2c6beaf7663ed84aa82263d430d4c56f975124
t7l <tobias.siebenlist@gmail.com>
2015-06-28 11:05:25 +0200

    some fixes...

--- krams/geolocation.py
+++ krams/geolocation.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from geopy.geocoders import GoogleV3
 import flickrapi
+import json
 
 api_key = u'f58f073c2e4b81e2ea674ba21cf637c7'
 api_secret = u'01c00f92fc58fd2f'
@@ -26,32 +27,36 @@ class Spot(Base):
 def get_lat_long(location_string):
     geolocator = GoogleV3()
     location = geolocator.geocode(location_string)
-    return (location.latitude, location.longitude)
+    return (location.__unicode__(), location.latitude, location.longitude)
 
-def get_flickr_photos(lat, lon, radius, number_of_pages):
+def get_flickr_photos(lat, lon, location_name, radius, number_of_pages):
     lat, lon = float(lat), float(lon)
     data = flickr.photos.search(lat=lat, lon=lon, radius=radius, radius_units='km', privacy_filter=1, license='1,2,3,4,5,6,7', content_type=1, extras='description,license,owner_name,original_format,geo,tags,machine_tags,views,url_o,count_faves,count_comments,visibility,usage,rotation',per_page=250,page=1,is_geo=True)
     photos = data['photos']['photo']
     page = 1
     while page < number_of_pages:
         page += 1
+        print "Flickr results, page {}...".format(page)
         if int(data['photos']['page']) < int(data['photos']['pages']):
             next_page = flickr.photos.search(lat=lat, lon=lon, radius=30, radius_units='km', privacy_filter=1, license='1,2,3,4,5,6,7', content_type=1, extras='description,license,owner_name,original_format,geo,tags,machine_tags,views,url_o,count_faves,count_comments,visibility,usage,rotation',per_page=250,page=page,is_geo=True)
             if not next_page.get('photos'):
                 continue
             photos.extend(next_page['photos']['photo'])
-    flickr_photos_to_db(photos)
+        print "{} photos so far...".format(len(photos))
+    flickr_photos_to_db(photos, lat, lon, location_name)
 
-def flickr_photos_to_db(photos):
+def flickr_photos_to_db(photos, lat, lon, location_name):
     from sqlalchemy.orm import scoped_session, sessionmaker, Query
     db_session = scoped_session(sessionmaker(bind=engine))
-    db_object = Spot(lat=lat, lon=lon, location_name = '', json_result=json.dumps(photos))
+
+    db_object = Spot(lat=lat, lon=lon, location_name = location_name, json_result=json.dumps(photos))
     db_session.add(db_object)
     db_session.commit()
 
 if __name__ == '__main__':
     cities = [u'Düsseldorf']
     for city in cities:
-        city_lat, city_lon = get_lat_long(city)
-        duesseldorf_photos = get_flickr_photos(lat=city_lat, lon=city_lon, radius=30, number_of_pages=10)
-        flickr_photos_to_db(city_photos)
+        print "Working on...", city
+        location_name, city_lat, city_lon = get_lat_long(city)
+        city_photos = get_flickr_photos(lat=city_lat, lon=city_lon, location_name=location_name, radius=30, number_of_pages=2)
+        flickr_photos_to_db(photos=city_photos, lat=city_lat, lon=city_lon, location_name=location_name)
